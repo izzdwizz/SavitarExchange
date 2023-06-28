1fcccd55052bc1fcc9ef86e0f1136f6e2b1d2091
Izuchukwu Uchendu <izuchukwu.uchendu@gmail.com>
2023-06-28 18:09:18 +0100

    signin page

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import SignUp from './assets/currency-coin-reward-box-6743487-5580576.png';
 
 const SignIn = () => {
-	return <div>SignIn</div>;
+	return (
+		<>
+			{' '}
+			<div className='relative w-full h-screen grid grid-cols-2 overflow-hidden'>
+				<div className=' bg-slate-900 min-h-screen flex justify-center items-center'>
+					<h1 className='w-full fixed top-4 left-4 justify-start text-left text-3xl font-bold text-[#e7a619]'>
+						enodatio.{' '}
+					</h1>
+					<img src={SignUp} alt='SignIn-img' />
+				</div>
+
+				<div className='bg-slate-700 min-h-screen'></div>
+			</div>
+			;
+		</>
+	);
 };
 
 export default SignIn;
